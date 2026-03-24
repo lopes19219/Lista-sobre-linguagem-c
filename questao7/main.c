664ba1294a57d2eb9ec245fc712377578bb8300a
lopes19219 <gcjlopes@gmail.com>
2026-03-23 21:43:30 -0300

    questao7

---
+++ questao7/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+    int quadrado;
+   
+   printf("Digite um numero: ");
+   scanf("%d", &num);
+   
+   quadrado = num * num;
+   
+   printf("O quadrado do seu numero é: %d\n", quadrado);
+   
+   return 0;
+}
