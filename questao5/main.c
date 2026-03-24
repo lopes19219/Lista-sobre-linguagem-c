65830d1fda110a343af0d5a14b0a0ad3a3091836
lopes19219 <gcjlopes@gmail.com>
2026-03-23 21:33:29 -0300

    questao5

---
+++ questao5/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+
+    printf("Digite seu numero: ");
+    scanf("%d", &num);
+
+    printf("O sucessor do seu número é: %d\n", num + 1);
+
+    return 0;
+}
